de0025321a9adf6cd63acab41a78338a92f96c43
Cleiton Trentin <cleiton.trentin@gmail.com>
2024-05-16 17:01:08 -0300

    Acesso ao jdbc h2

--- ws-sts/tcc/src/main/java/com/gerenciamentoDeCartao/tcc/entities/Users.java
+++ ws-sts/tcc/src/main/java/com/gerenciamentoDeCartao/tcc/entities/Users.java
@@ -3,12 +3,18 @@ package com.gerenciamentoDeCartao.tcc.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class Users implements Serializable{
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	@jakarta.persistence.Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
     private String nome;
     private String login;
